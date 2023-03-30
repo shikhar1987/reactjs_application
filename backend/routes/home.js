180afdd760bbeb2ff49adc0732959dfb7f2b2374
jwang25 <33610917+jwang25@users.noreply.github.com>
2023-03-30 20:21:42 +1000

    Delete backend directory

--- backend/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'CAB230 Haonan Wang n9475389' });
-});
-
-module.exports = router;
