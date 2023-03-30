ce871f97cf9cc72d26a5ba680b198c0c9b97023d
jwang25 <33610917+jwang25@users.noreply.github.com>
2023-03-30 20:18:01 +1000

    Update splunk-instrumentation.js

--- frontend/src/splunk-instrumentation.js
+++ frontend/src/splunk-instrumentation.js
@@ -1,7 +1,7 @@
 import SplunkOtelWeb from '@splunk/otel-web';
 SplunkOtelWeb.init({
-    beaconUrl: "https://rum-ingest.us0.signalfx.com/v1/rum",
-    rumAuth: "a5OVE71NXiRkCYxH34_Fbw",
-    app: "stock_app",
-    environment: "johnw_env"
+   beaconUrl: "https://rum-ingest.us1.signalfx.com/v1/rum",
+   rumAuth: "lIcnEUBMgRzeVMoePk_0Mg",
+   app: "stock_app",
+   environment: "johnw_env"
 });
