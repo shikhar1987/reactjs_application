6cb67be9ed6ee6e3df8c27f7f49fb9e25bdf60fe
jwang25 <33610917+jwang25@users.noreply.github.com>
2023-03-31 02:47:49 +1000

    Update splunk-instrumentation.js

--- src/splunk-instrumentation.js
+++ src/splunk-instrumentation.js
@@ -1,7 +1,7 @@
 import SplunkOtelWeb from '@splunk/otel-web';
 SplunkOtelWeb.init({
-    beaconUrl: process.env.beaconUrl,
-    rumAuth: process.env.rumAuth,
+    beaconUrl: process.env.REACT_APP_beaconUrl,
+    rumAuth: process.env.REACT_APP_rumAuth,
     app: "stock_app",
     environment: "johnw_env",
     globalAttributes: {
